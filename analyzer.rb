65cfad77b5bb4190e76cd6e84658bc2f5b4b0487
lm0x <depkey98@gmail.com>
2017-06-11 15:11:14 +0900

    なんとかして

--- analyzer.rb
+++ analyzer.rb
@@ -5,7 +5,10 @@ def AnalyzeReport mail
 	
 	r = @doc.xpath('//table[@width="750px"]/tbody/tr[2]/td/table[@width="700px"]/tbody/tr')
 	
-	reports = {}
+	# ぽえ～
+
+	reports = []
+	report = {}
 	agent = nil
 	i = 0 # for reports
 	j = 0 # for linked portals
@@ -26,6 +29,7 @@ def AnalyzeReport mail
 		
 			# <div>DAMAGE REPORT</div>
 			unless s.xpath('td[@style="font-size: 17px; padding-bottom: .2em; border-bottom: 2px solid #403F41;"]').empty?
+				report = {}
 				# puts s.to_html
 			end
 		
@@ -35,12 +39,12 @@ def AnalyzeReport mail
 			unless s.xpath('td[@style="font-size: 17px;padding-bottom: .2em;border-bottom: 2px solid #403F41;text-transform: uppercase;"]').empty?
 				i += 1
 				j = 0
-				reports[i] = {}
+				reports.push report
 			end
 
 			# ポータル情報(名前,アドレス)
 			unless s.xpath('td[@style="padding-top: 1em; padding-bottom: 1em;"]').empty?
-				reports[i] = {:portal => {
+				report = {:portal => {
 					:name => s.xpath('td[@style="padding-top: 1em; padding-bottom: 1em;"]/div[1]').text,
 					:intel => s.xpath('td[@style="padding-top: 1em; padding-bottom: 1em;"]/div/a').attribute('href').to_s
 					}}
@@ -49,19 +53,19 @@ def AnalyzeReport mail
 		
 			# ポータル情報(画像)
 			unless s.xpath('td[@style="overflow: hidden;"]/table[@cellpadding="0"][@cellspacing="0"][@border="0"]').empty?
-				reports[i][:portal][:photo] = s.xpath('//div[@style="width: auto; height: 160px; float: left; display: inline-block;"]/img').attribute("src").to_s
-				reports[i][:portal][:intel_image] = s.xpath('//div[@style="width: auto; height: 160px; float: left; display: inline-block; overflow:hidden;"]/img').attribute("src").to_s
+				report[:portal][:photo] = s.xpath('//div[@style="width: auto; height: 160px; float: left; display: inline-block;"]/img').attribute("src").to_s
+				report[:portal][:intel_image] = s.xpath('//div[@style="width: auto; height: 160px; float: left; display: inline-block; overflow:hidden;"]/img').attribute("src").to_s
 			end
 		
 			# ダメージ情報
 			unless s.xpath('td[@style="padding: 1em 0;"]').empty?
 				# ダメージ詳細
 				about_damage = s.xpath('td[@style="padding: 1em 0;"]/table/td[@width="400px"]/div')
-				reports[i][:attacked_by] = about_damage.xpath('span[@style="color: #428F43;"]').text
+				report[:attacked_by] = about_damage.xpath('span[@style="color: #428F43;"]').text
 				
 				# DAMAGE:9 Links destroyed by godiego at 10:00 hrs GMTNo remaining Resonators detected on this Portal.
 				about_damage.text.match(/([0-9]{1}) (.+) destroyed by .+ at ([0-9]{1,2}:[0-9]{1,2}) hrs GMT/) do |m|
-					reports[i][:portal][:damage] = {
+					report[:portal][:damage] = {
 						:count => m[1].to_i,
 						:type => m[2].gsub(/s$/, ""),
 						:date => m[3]
@@ -73,7 +77,7 @@ def AnalyzeReport mail
 				
 				# STATUS:Level 1Health: 3%Owner: tujiyan
 				portal_status = s.xpath('td[@style="padding: 1em 0;"]/table/td[2]/div')
-				reports[i][:status] = {
+				report[:status] = {
 					:level => portal_status.text.match(/Level ([1-8])/)[1].to_i,
 					:health => portal_status.text.match(/Health: ([0-9]{1,})%/)[1].to_i,
 					:owner => portal_status.text.match(/Owner: (.+)/)[1]
@@ -84,7 +88,7 @@ def AnalyzeReport mail
 			unless s.xpath('td/table[@cellpadding="0"][@cellspacing="0"][@border="0"][@width="700px"]').empty? or s.text == "LINKS DESTROYED" or s.text.match /on this Portal./
 				
 				if j == 0
-					reports[i][:linked_portals] = []
+					report[:linked_portals] = []
 				end
 		
 				# intel
@@ -94,7 +98,7 @@ def AnalyzeReport mail
 				end
 		
 				s.xpath('td/table[@cellpadding="0"][@cellspacing="0"][@border="0"][@width="700px"]/td').text.match(/(.+): (.+)/) do |m|
-					reports[i][:linked_portals][j] = {
+					report[:linked_portals][j] = {
 						:intel => intel,
 						:name => m[1],
 						:address => m[2]
@@ -106,54 +110,6 @@ def AnalyzeReport mail
 			return 0
 		end
 	end
+	reports.push report
 	return {:reports => reports, :agent => agent}
 end
-
-
-=begin
-pp reports =>
-{:agent=>{:codename=>"Trauminator", :faction=>"Resistance", :level=>16},
- 0=>
-  {:portal=>
-    {:name=>"\u5C0F\u5C71\u4E0A\u7DCF\u753A\u5730\u8535\u5C0A",
-     :intel=>
-      "https://www.ingress.com/intel?ll=35.042906,135.759873&pll=35.042906,135.759873&z=19",
-     :photo=>
-      "http://lh6.ggpht.com/zOoVkoPoykPg1l5LBPJLjVn9L4hQHEnza1quuXrlqsXCq2_Rcj_AXG82tfh-aITK12IwMdk-c7f444AyQ11D",
-     :intel_image=>
-      "http://maps.googleapis.com/maps/api/staticmap?center=35.042906,135.759973&zoom=19&size=700x160&style=visibility:on%7Csaturation:-50%7Cinvert_lightness:true%7Chue:0x131c1c&style=feature:water%7Cvisibility:on%7Chue:0x005eff%7Cinvert_lightness:true&style=feature:poi%7Cvisibility:off&style=feature:transit%7Cvisibility:off&markers=icon:http://commondatastorage.googleapis.com/ingress.com/img/map_icons/marker_images/hum_8res.png%7Cshadow:false%7C35.042906,135.759873&client=gme-nianticinc&signature=0txbHpc9mrj0ElvOyDZcPELoPk8=",
-     :damage=>{:count=>1, :type=>"Link", :date=>"14:38"}},
-   :linked_portals=>
-    [{:intel=>
-       "https://www.ingress.com/intel?ll=35.043271,135.759122&pll=35.043271,135.759122&z=19",
-      :name=>
-       "\u30BD\u30D5\u30C8\u30D0\u30F3\u30AF\u5317\u5927\u8DEF\u70CF\u4E38",
-      :address=>
-       "15 Koyamakamifusach\u014D, Kita-ku, Ky\u014Dto-shi, Ky\u014Dto-fu 603-8143, Japan"}],
-   :attacked_by=>"Alice95",
-   :status=>{:level=>6, :health=>91, :owner=>"yukikaze846"}},
- 1=>
-  {:portal=>
-    {:name=>
-      "\u30BD\u30D5\u30C8\u30D0\u30F3\u30AF\u5317\u5927\u8DEF\u70CF\u4E38",
-     :intel=>
-      "https://www.ingress.com/intel?ll=35.043271,135.759122&pll=35.043271,135.759122&z=19",
-     :photo=>
-      "http://lh6.ggpht.com/zOoVkoPoykPg1l5LBPJLjVn9L4hQHEnza1quuXrlqsXCq2_Rcj_AXG82tfh-aITK12IwMdk-c7f444AyQ11D",
-     :intel_image=>
-      "http://maps.googleapis.com/maps/api/staticmap?center=35.042906,135.759973&zoom=19&size=700x160&style=visibility:on%7Csaturation:-50%7Cinvert_lightness:true%7Chue:0x131c1c&style=feature:water%7Cvisibility:on%7Chue:0x005eff%7Cinvert_lightness:true&style=feature:poi%7Cvisibility:off&style=feature:transit%7Cvisibility:off&markers=icon:http://commondatastorage.googleapis.com/ingress.com/img/map_icons/marker_images/hum_8res.png%7Cshadow:false%7C35.042906,135.759873&client=gme-nianticinc&signature=0txbHpc9mrj0ElvOyDZcPELoPk8=",
-     :damage=>{:count=>2, :type=>"Link", :date=>"14:38"}},
-   :linked_portals=>
-    [{:intel=>
-       "https://www.ingress.com/intel?ll=35.042906,135.759873&pll=35.042906,135.759873&z=19",
-      :name=>"\u5C0F\u5C71\u4E0A\u7DCF\u753A\u5730\u8535\u5C0A",
-      :address=>
-       "2 Koyamakamifusacho, Kita Ward, Kyoto, Kyoto Prefecture, Japan"},
-     {:intel=>
-       "https://www.ingress.com/intel?ll=35.043487,135.758256&pll=35.043487,135.758256&z=19",
-      :name=>"\u4EAC\u90FD\u8056\u5F92\u6559\u4F1A",
-      :address=>
-       "50 Koyamakamifusacho, Kita Ward, Kyoto, Kyoto Prefecture, Japan"}],
-   :attacked_by=>"Alice95",
-   :status=>{:level=>1, :health=>15, :owner=>"Trauminator"}}}
-=end
